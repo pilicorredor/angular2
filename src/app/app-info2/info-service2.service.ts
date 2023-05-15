ca58e5e74a231981450a9dd4273617164b7300d1
pili111 <93451450+pilicorredor@users.noreply.github.com>
2023-05-15 11:14:44 -0500

    Update info-service2.service.ts

--- src/app/app-info2/info-service2.service.ts
+++ src/app/app-info2/info-service2.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class InfoService2Service {
-  private url:string = "http://10.4.72.127:30002/api/v1/Backend_info";
+  private url:string = "http://192.168.1.19:30005/api/v1/Backend_info";
 
   constructor( private http:HttpClient ) { }
 
